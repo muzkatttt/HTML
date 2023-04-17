85d7ac9d335d778930aaacd95f66c6640aa863a1
Katerina <emrodionova@bk.ru>
2023-04-17 22:21:55 +0300

    added sem 4 resume

---
+++ all_resume/myResume.php
@@ -0,0 +1,40 @@
+<?php
+$name = 'Ekaterina Rodionova';
+$profession = 'Engineer';
+$city_country = 'Agryz, Russia';
+$e_mail = 'emrodionova@bk.ru';
+$phone = '+7 987 233 00 36';
+$age_years =  2023 - 1986;
+$age_days = $age_years * 365;
+$skills = [
+    [
+        'name' => 'SAP',
+        'percent' => 100,
+    ],
+    [
+        'name' => 'Python',
+        'percent' => 50,
+    ],
+    [
+        'name' => 'MS Office',
+        'percent' => 99,
+    ],
+    [
+        'name' => 'Lean Production',
+        'percent' => 99,
+    ],
+    
+];
+$job = [
+            'job_name' => ['Russian Railways', 'RA Dvizhok'],
+            'job_data' => ['2014 - present time', '2007 - 2010'],
+            'job_desc' => ['Engineer', 'Manager'],
+            'job_about' => ['Rationing of labor. Consulting on remuneration issues. Employee bonuses. Introduction of lean manufacturing tools', 'Outdoor advertising sales'],
+];
+
+$education = [
+            'education_name' => ['GeekBrains', 'Ural State University of Railway Transport', 'Samara State University of Railway Transport', 'Udmurt State University'],
+            'education_data' => ['2022 - present time', '2020', '2018', '2011'],
+            'education_degree' => ['Developer', 'Economist', 'Locomotives', 'Documentologist'],
+];
+?>
